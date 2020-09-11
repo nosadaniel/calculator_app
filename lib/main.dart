6236af7d17211bc13f352f703653659efc60e5fe
nosadaniel <nosadaniel02@gmail.com>
2020-09-11 09:46:27 +0100

    commit made by sam

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Color(0xFF221111),
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+//06082e
+//11133f
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    margin: EdgeInsets.all(10.0),
+                    padding: EdgeInsets.all(60.0),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFF0A0E21),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    margin: EdgeInsets.all(10.0),
+                    padding: EdgeInsets.all(60.0),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
